021e21cf0fbea68d131d55453c7891f9406a14fc
Siddharth Bhogla <thebhogla@gmail.com>
2025-06-07 07:33:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: import.meta.env.PORT? parseInt(import.meta.env.PORT) : 4000,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
