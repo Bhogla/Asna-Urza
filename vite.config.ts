d7ac7ec251c898a0d17086de735a48587a3dc515
Siddharth Bhogla <thebhogla@gmail.com>
2025-06-07 07:54:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: import.meta.env.PORT? parseInt(import.meta.env.PORT) : 4000,
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    proxy: {
+      // Proxy requests with a '/api' prefix to your express server
+      '/api': {
+        target: 'http://localhost:4000', // The address of your express server
+        changeOrigin: true, // Recommended for virtual hosts
+        // Optional: rewrite path if you don't want the /api prefix passed on
+        // rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
   },
 });
