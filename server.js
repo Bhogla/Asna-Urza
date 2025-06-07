97a3b99ae733e8e1510f0cbd903c3d15b71a0f0b
Siddharth Bhogla <thebhogla@gmail.com>
2025-06-07 07:56:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express') 
- const app = express() 
- const port = process.env.PORT || 4000 
- 
- app.get('/', (req, res) => { 
-   res.send('Hello World!') 
- }) 
- 
- app.listen(process.env.PORT || port, () => { 
-   console.log(`Example app listening on port ${port}`) 
- })
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000; // Use port 4000 for the backend
+
+// This is your API endpoint
+app.get('/api/hello', (req, res) => {
+  res.send('Hello from the Express Server!');
+});
+
+app.listen(port, () => {
+  console.log(`✅ Express server listening on port ${port}`);
+});
